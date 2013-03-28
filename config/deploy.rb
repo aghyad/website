7886dfaf54dbefe6b3f02d48adef26f7b20dd962
asaleh <asaleh@clickhere.com>
2013-03-27 22:33:47 -0500

    added capistrano + deployment settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,68 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :stages, %w(production)
+set :default_stage, "production"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+#require "rvm/capistrano"
+#require "bundler/capistrano"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+require 'capistrano/ext/multistage'
+
+#require "whenever/capistrano"
+
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+
+role :web, "198.211.106.13"                     # Your HTTP server, Apache/etc
+role :app, "198.211.106.13"                     # This may be the same as your `Web` server
+role :db,  "198.211.106.13", :primary => true   # This is where Rails migrations will run
+
+set :application, "website"
+set :user, "root"
+#set :password, "ubzlvsoovnij"
+#set :deploy_via, :remote_cache
+set :deploy_via, :checkout
+set :use_sudo, false
+set :rails_env, ARGV[0]
+set :deploy_to, "/var/www/apps/#{application}/#{rails_env}"    ### activate for production deployment
+
+set :scm, "git"
+set :repository, "git@github.com:aghyad/website.git"
+set :branch, "dev"
+set :scm_verbose, true
+
+namespace :deploy do
+  desc 'Create default database'
+  task :createdb, :roles => :db, :only => { :primary => true } do
+    rake = fetch(:rake, "bundle exec rake")
+    rails_env = ARGV[0]
+    migrate_env = fetch(:migrate_env, "dev")
+    migrate_target = fetch(:migrate_target, :latest)
+    
+    directory = case migrate_target.to_sym
+    when :current then current_path
+    when :latest  then latest_release
+    else raise ArgumentError, "unknown migration target #{migrate_target.inspect}"
+    end
+    
+    run "cd #{directory} && #{rake} RAILS_ENV=#{rails_env} #{migrate_env} db:create"
+    run "cd #{directory} && #{rake} RAILS_ENV=#{rails_env} #{migrate_env} db:migrate"
+    run "cd #{directory} && #{rake} RAILS_ENV=#{rails_env} #{migrate_env} db:seed"
+  end
+  
+  ### activate ONLY for staging ##############################
+  desc "Disallow search engine indexing"
+  task :add_robots do
+    if rails_env == "dev"
+      run "touch #{deploy_to}/current/robots.txt; /bin/echo -en 'User-agent: *\\nDisallow: /' >> #{deploy_to}/current/robots.txt;"
+    end
+  end
+  
+  desc "Restart Application"
+  task :restart do
+    #run "chown -R apache:apache #{deploy_to}"
+    #run "touch #{deploy_to}/current/tmp/restart.txt"
+    run "/etc/init.d/nginx restart"
+  end
+end
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+after "deploy:restart", "deploy:cleanup"
