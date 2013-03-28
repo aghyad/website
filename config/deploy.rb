bc408f578c282e0dd9fca266c36a9d76a8c1f940
asaleh <asaleh@clickhere.com>
2013-03-28 11:17:45 -0500

    added cap rvm commands for setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,17 @@
 set :stages, %w(production)
 set :default_stage, "production"
 
+### The following is necessary for RVM and cap detection:
+set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
+set :rvm_install_ruby_params, '--1.9'      # for jruby/rbx default to 1.9 mode
+set :rvm_install_pkgs, %w[libyaml openssl] # package list from https://rvm.io/packages
+set :rvm_install_ruby_params, '--with-opt-dir=/usr/local/rvm/usr' # package support
+before 'deploy:setup', 'rvm:install_rvm'   # install RVM
+#before 'deploy:setup', 'rvm:install_pkgs'  # install RVM packages before Ruby
+before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset, or:
+before 'deploy:setup', 'rvm:create_gemset' # only create gemset
+#before 'deploy:setup', 'rvm:import_gemset' # import gemset from file
+
 require "rvm/capistrano"
 require "bundler/capistrano"
 
@@ -21,7 +32,7 @@ set :user, "root"
 #set :deploy_via, :remote_cache
 set :deploy_via, :checkout
 set :use_sudo, false
-set :rails_env, ARGV[0]
+set :rails_env, "production"
 set :deploy_to, "/var/www/apps/#{application}/#{rails_env}"    ### activate for production deployment
 
 set :scm, "git"
