c32a60f6bbbbb79dedf4ed552f5efb3ddb195706
asaleh <asaleh@clickhere.com>
2013-05-23 16:08:46 -0500

    more features and fixing bugs

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130523210533) do
+
+  create_table "contacts", :force => true do |t|
+    t.string   "contact_name"
+    t.string   "contact_email"
+    t.string   "contact_phone"
+    t.string   "contact_title"
+    t.text     "contact_message"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
 end
